8eea6312275e1acc45a6822f91a0ee8afc88f516
MUGUNTHAN P <146167205+Muguntha12@users.noreply.github.com>
2024-07-27 14:36:34 +0530

    dll reverse

---
+++ linkedlist/Double/DLL_reverse.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Node{
+  int data;
+  Node next;
+  Node prev;
+  
+  Node(int data)
+  {
+    this.data=data;
+    this.next=null;
+    this.prev=null;
+  }
+}
+class doubl{
+  Node head;
+  Node tail;
+  
+  doubl()
+  {
+    head=null;
+    tail=null;
+  }
+  public void insert(int data)
+  {
+    Node nn=new Node(data);
+    if(head==null)
+    {
+      head=nn;
+      tail=nn;
+    }
+    else
+    {
+      tail.next=nn;
+      nn.prev=tail;
+      tail=nn;
+    }
+  }
+  public void reverse()
+  {
+    Node temp=tail;
+    while(temp!=null)
+    {
+      System.out.printf("%d ",temp.data);
+      temp=temp.prev;
+    }
+  }
+}
+class DLL_reverse
+{
+  public static void main(String[] args)
+  {
+    Scanner ob=new Scanner(System.in);
+    doubl db=new doubl();
+    int val=0;
+    while(val!=-1)
+    {
+      val=ob.nextInt();
+      if(val==-1)break;
+      db.insert(val);
+    }
+    db.reverse();
+  }
+}
