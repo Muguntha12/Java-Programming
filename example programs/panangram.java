c963b0a57958bd08b89260ef5a3573e03750ed23
MUGUNTHAN P <146167205+Muguntha12@users.noreply.github.com>
2024-07-31 11:03:05 +0530

    Add files via upload

---
+++ example programs/panangram.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class panangram 
+{
+    	public static void main(String[] args) {
+		Runnable mega=() -> {
+		try 
+        {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String str1=str.toLowerCase();
+		int[] arr=new int[26];
+		for(int i=0;i<26;i++)
+			arr[i]=0;
+		int m=0,count=0;
+		sc.close();
+		for(int i=0;i<str1.length();i++)
+		{
+			 char c=str1.charAt(i);
+			    int a=c;
+				if(c>=97&&c<=122&&c!=32)
+				{
+					m=a-97;
+					arr[m]+=1;
+					
+				}
+			}
+		
+		for(int i=0;i<26;i++)
+		{
+			if(arr[i]==0) {
+				count=1;
+				break;}
+		}
+		Thread.sleep(200);
+		if(count==1)
+			System.out.print("Not a panagram");
+		else
+			System.out.print("Panagram");
+		}
+		
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	};
+	Thread f=new Thread(mega);
+	f.start();;
+
+}
+}
