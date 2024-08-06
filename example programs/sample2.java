b794da72a631b87cdfc2eda1d7b414edb2aa5359
MUGUNTHAN P <146167205+Muguntha12@users.noreply.github.com>
2024-08-06 21:27:48 +0530

    Add files via upload

---
+++ example programs/sample2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class sample2
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int size = obj.nextInt();
+        int arr[] = new int[size];
+        int i =0;int sum=0;int j=0;int b=0;
+        for(i=0;i<size;i++)
+        {
+            arr[i]=obj.nextInt();
+        }
+        try
+        {
+            for(i=0;i<size;i++)
+            {
+                for(j=i+1;j<size;j++)
+                {
+                    if(arr[i]==arr[j])
+                    {
+                    b=arr[i];
+                    sum=sum+1;
+                    sum=sum/0;
+                    }
+                }
+            }
+            if(sum==0)
+            {
+            System.out.println("Duplicates not found");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.print("Duplicates found: "+b);}
+    }
+}
