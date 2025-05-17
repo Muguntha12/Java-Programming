110512e3ad2aae81df287733a27903462334fe80
MUGUNTHAN P <146167205+Muguntha12@users.noreply.github.com>
2025-05-17 09:08:34 -0800

    Create Substring.java

---
+++ example programs/Substring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Substring
+{
+    public static void main(String[]args)
+    {
+        Scanner obj=new Scanner(System.in);
+        String n=obj.nextLine();
+         System.out.print(n.substring(0,n.indexOf(" ")));
+        
+    }
+}
