a7ce4b30a4b26eed2241b7cbb984904ac9074f07
MUGUNTHAN P <146167205+Muguntha12@users.noreply.github.com>
2025-05-19 06:39:55 -0800

    Create mobile.java

---
+++ example programs/mobile.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class  mobile{
+    void number()
+    {
+        System.out.print("8973389716");
+    }
+    void incoming()
+    {
+        System.out.print("incoming yes"); 
+        outgoinng();
+    }
+    void outgoinng()
+    {
+        System.out.print("outgoing no");
+    }
+
+public static void main(String[]args)
+{
+    Mobile data=new Mobile();
+    data.incoming();
+}
+}
